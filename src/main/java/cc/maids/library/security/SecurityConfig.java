08762b4ced24bfecf0c77764fb97be631920a41a
ahmedyoussof <ahmed.zakieldin@musala.com>
2024-07-31 23:49:17 +0300

    configure basic authentication.

---
+++ src/main/java/cc/maids/library/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package cc.maids.library.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+  private static final String API_URL = "/api";
+  public static final String INCLUDE_ALL_URL = "/**";
+  private static final String ADMIN_ROLE = "ADMIN_ROLE";
+
+  @Bean
+  public SecurityFilterChain adminFilterChain(HttpSecurity http) throws Exception {
+    return http
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(withDefaults())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .securityMatcher(API_URL + INCLUDE_ALL_URL)
+        .authorizeHttpRequests(auth ->
+            auth.anyRequest().hasRole(ADMIN_ROLE)
+        )
+        .httpBasic(Customizer.withDefaults())
+        .build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return new InMemoryUserDetailsManager(
+        User.withUsername("admin")
+            .password("{noop}password")
+            .roles(ADMIN_ROLE)
+            .build()
+    );
+  }
+}
